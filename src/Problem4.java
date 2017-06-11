eeffbe1b54627113dff3f5ad806b0e722c12652c
Kontor <domrycz@gmail.com>
2017-06-11 19:38:09 +0200

    better implementation of problem 4

--- src/Problem4.java
+++ src/Problem4.java
@@ -1,38 +1,39 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Problem4 {
 
     public void solution() {
-        List<Integer> integers = new ArrayList<>();
+        Integer integers[] = new Integer[4];
         Scanner userInput = new Scanner(System.in);
         System.out.println("## Creating the largest possible number ##");
         System.out.println("Enter four integers to the list: ");
 
-        do {
+        for(int i = 0; i < integers.length; i++) {
             try {
-                integers.add(Integer.valueOf(userInput.nextLine()));
+                integers[i] = Integer.valueOf(userInput.nextLine());
             } catch (NumberFormatException nfe) {
                 System.out.println("This is not an integer!");
             }
-        } while(integers.size() < 4);
-
-        System.out.println("Your list: " + integers + "\n");
+        }
+        System.out.println("Your numbers: " + Arrays.toString(integers) + "\n");
+        Arrays.sort(integers);
         String result = "";
+        String strings[] = new String[4];
 
-        List<String> intToString = new ArrayList<>();
-        for(int i = 0; i < integers.size(); i++) {
-            intToString.add(integers.get(i).toString());
+        int count = 0;
+        for(int i = strings.length - 1; i >= 0; i--) {
+            strings[i] = integers[count].toString();
+            count++;
         }
 
         for(Integer i = 9; i > -1; i--) {
-            for(int j = 0; j < intToString.size(); j++) {
-                if(intToString.get(j).equals(i.toString()))
-                    result = result + intToString.get(j);
+            for(int j = 0; j < strings.length; j++) {
+                if(strings[j].charAt(0) == i.toString().charAt(0))
+                    result += strings[j];
             }
         }
 
-        System.out.println("The largest number: " + result);
+        System.out.println("The largest possible number is: " + result);
     }
 }
