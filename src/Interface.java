d1325771f850f07a07afa813c197c34250ced321
Kontor <domrycz@gmail.com>
2017-06-16 20:49:34 +0200

    Code cleaning

--- src/Interface.java
+++ src/Interface.java
@@ -13,31 +13,37 @@ public class Interface {
             System.out.println("(2) Combining two lists (string & integer)");
             System.out.println("(3) 100 Fibonacci numbers");
             System.out.println("(4) The largest number");
+            System.out.println("(5) \"Result is always 100\"");
             System.out.println("(x) Exit");
 
             System.out.print("Your choice: ");
             userChoice = userInput.nextLine();
 
-        switch (userChoice) {
-            case "1":
-                Problem1 problem1 = new Problem1();
-                problem1.solution();
-                break;
-            case "2":
-                Problem2 problem2 = new Problem2();
-                problem2.solution();
-                break;
-            case "3":
-                Problem3 problem3 = new Problem3();
-                problem3.solution();
-            case "4":
-                Problem4 problem4 = new Problem4();
-                problem4.solution();
-            case "x":
-                break;
-            default:
-                System.out.println("Check available options one more time :)");
-        }
+            switch (userChoice) {
+                case "1":
+                    Problem1 problem1 = new Problem1();
+                    problem1.solution();
+                    break;
+                case "2":
+                    Problem2 problem2 = new Problem2();
+                    problem2.solution();
+                    break;
+                case "3":
+                    Problem3 problem3 = new Problem3();
+                    problem3.solution();
+                    break;
+                case "4":
+                    Problem4 problem4 = new Problem4();
+                    problem4.solution();
+                    break;
+                case "5":
+                    System.out.println("I'm sorry but I'm still not able to solve this problem :(");
+                    break;
+                case "x":
+                    break;
+                default:
+                    System.out.println("Check available options one more time :)");
+            }
 
         } while(!userChoice.equals("x"));
     }
--- src/Problem4.java
+++ src/Problem4.java
@@ -16,24 +16,32 @@ public class Problem4 {
                 System.out.println("This is not an integer!");
             }
         }
+
         System.out.println("Your numbers: " + Arrays.toString(integers) + "\n");
         Arrays.sort(integers);
-        String result = "";
-        String strings[] = new String[4];
+        String strings[] = intToString(integers);
 
+        System.out.println("The largest possible number is: " + makeNumber(strings));
+    }
+
+    private String[] intToString(Integer[] ints) {
+        String strings[] = new String[ints.length];
         int count = 0;
-        for(int i = strings.length - 1; i >= 0; i--) {
-            strings[i] = integers[count].toString();
+        for(int i = ints.length - 1; i >= 0; i--) {
+            strings[i] = ints[count].toString();
             count++;
         }
+        return strings;
+    }
 
+    private String makeNumber(String[] nums) {
+        String number = "";
         for(Integer i = 9; i > -1; i--) {
-            for(int j = 0; j < strings.length; j++) {
-                if(strings[j].charAt(0) == i.toString().charAt(0))
-                    result += strings[j];
+            for(int j = 0; j < nums.length; j++) {
+                if(nums[j].charAt(0) == i.toString().charAt(0))
+                    number += nums[j];
             }
         }
-
-        System.out.println("The largest possible number is: " + result);
+        return number;
     }
 }
